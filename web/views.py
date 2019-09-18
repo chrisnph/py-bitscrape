4d59969593b5fe5abaee95a29815ca6f013b0d45
Chris <ngpohhhaw@yahoo.com>
2019-09-18 20:51:43 +0800

    removed Django

--- web/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# fetch from json file and render to index
-
-def index(request):
-    threads = filter(None,Scraper().thread_parsed)
-    return render(request, 'home/index.html', {'threads': threads})
