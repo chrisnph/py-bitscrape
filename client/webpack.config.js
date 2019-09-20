e77d7d4c32f9f268bdc85795baa4119d37263251
Chris <ngpohhhaw@yahoo.com>
2019-09-20 16:42:22 +0800

    remove conflicting .babelrc

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "index.js",
+  entry: ["babel-polyfill", "index.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.bundle.js"
