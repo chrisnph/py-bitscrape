4ce7b97b3a62750759b11d72cad9be74f3ee66d0
Chris <ngpohhhaw@yahoo.com>
2019-09-27 02:05:52 +0800

    add data toggles,localStorage updates, data sync every 2mins

--- client/src/components/DataTables/DataRows.js
+++ client/src/components/DataTables/DataRows.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import jsonData from "./threads.json";
 import { setInterval } from "timers";
 
 const Rows = styled.div`
@@ -8,9 +7,14 @@ const Rows = styled.div`
 
   & .row {
     min-height: 60px;
-    max-height: 60px;
     display: flex;
     align-items: center;
+  }
+  & .row.datarow-header {
+    max-height: 60px;
+  }
+  & .wrapper-datarow.hidden {
+    display: none;
   }
   & .datarow {
     font-size: 18px;
@@ -23,79 +27,103 @@ const Rows = styled.div`
   & .datarow:not(.datarow-header) {
     font-size: 14px;
   }
-  & > div:nth-child(odd):not(.datarow-header) {
+  & > div:nth-child(odd):not(.datarow-header):not(.actionrow) {
     background-color: #eee;
+  }
+  & .datarow-info {
+    margin: 15px auto;
+    box-shadow: 2px -2px 6px #fff;
+    position: fixed;
+    z-index: 1;
+    width: 95%;
+    height: 90%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgb(248, 248, 248);
+    border-radius: 10px;
+    overflow-y: scroll !important;
+
+    & #info-close {
+      position: fixed;
+      top: 1.5%;
+      right: 1.5%;
+      color: red;
+      background: transparent;
+      font-size: 34px;
+      box-shadow: none;
+      border: none;
+      outline: none;
+      filter: drop-shadow(-1px 1px 2px #000);
+    }
+    & #info-close:hover {
+      filter: drop-shadow(-1px 1px 1px #000)
+    }
+
+    & .userimg {
+      max-width: 100%;
+    }
   }
 `;
 
 const DataRows = props => {
-  // json data state
-  const [dataRows, updatedataRows] = useState(jsonData);
+  let dataFiltered = props.dataRow.filter(f => {
+    return [
+      f["Post Date"].split(" ")[0].toLowerCase() === "today",
+      f["Post Date"].split(" ")[0].toLowerCase() !== "today"
+    ];
+  });
 
-  let dataDisplay = data => {
-    return data.map((row, index) => {
-      return (
-        <div key={index} className="row py-2">
-          <div className="col-md-5">
-            <a
-              href="#"
-              onClick={e => e.preventDefault()}
-              className="datarow datarow-title"
-            >
-              {row.title}
-            </a>
-          </div>
-          <div className="col-md-1">
-            <a
-              href={row.source}
-              target="_blank"
-              className="datarow datarow-source"
-            >
-              BitCoinTalk
-            </a>
-          </div>
-          <div className="col-md-2">{row.algo.join(" | ")}</div>
-          <div className="col-md-3">{row["Post Date"]}</div>
-          <div className="col-md-1">
-            <button className="btn btn-warning btn-sm">
-              <i className="fa fa-eye-slash" aria-hidden="true"></i>
-            </button>
-          </div>
+  const dataDisplay = dataFiltered.map((row, index) => {
+    return (
+      <div
+        key={index}
+        className={`row wrapper-datarow py-2 ${props.dataHidden.includes(
+          row.source
+        ) && "hidden"}`}
+      >
+        <div className="col-md-5">
+          <a
+            href="#"
+            onClick={(e) => props.toggleInfo({e, info: row.info, close: false})}
+            className="datarow datarow-title"
+          >
+            {row.title}
+          </a>
         </div>
-      );
-    });
-  };
-
-  // html data row state
-  const [displayRows, handleDisplayRows] = useState(dataDisplay(dataRows));
-
-  useEffect(() => {
-    setInterval(() => {
-      fetch("src/components/DataTables/threads.json", {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        }
-      })
-        .then(res => {
-          return res.json();
-        })
-        .then(newData => {
-          dataRows !== newData && handleDisplayRows(dataDisplay(newData));
-        });
-    }, 2000);
-  }, [dataRows]);
+        <div className="col-md-1">
+          <a
+            href={row.source}
+            target="_blank"
+            className="datarow datarow-source"
+          >
+            BitCoinTalk
+          </a>
+        </div>
+        <div className="col-md-2">{row.algo.join(" | ")}</div>
+        <div className="col-md-3">{row["Post Date"]}</div>
+        <div className="col-md-1">
+          <button
+            className="btn btn-warning btn-sm rounded-circle"
+            onClick={e => props.isVisible(e)}
+          >
+            <i className="fa fa-eye-slash" aria-hidden="true"></i>
+          </button>
+        </div>
+      </div>
+    );
+  });
 
   return (
-    <Rows className="container-fluid p-3 mt-3">
+    <Rows className="container-fluid mb-5">
       <div className="row datarow datarow-header py-2">
         <div className="col-md-5">Thread</div>
         <div className="col-md-1">Source</div>
         <div className="col-md-2">Algo</div>
-        <div className="col-md-2">Post Date</div>
+        <div className="col-md-2">Last Post</div>
         <div className="col-md-1"></div>
       </div>
-      {displayRows}
+      {dataDisplay}
     </Rows>
   );
 };
--- client/src/components/DataTables/DataTables.jsx
+++ client/src/components/DataTables/DataTables.jsx
@@ -1,11 +1,138 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import jsonData from "./threads.json";
 import DataRows from "./DataRows";
+import styled from "styled-components";
 
-const DataTables = props => {
+const Table = styled.div`
+  margin: 30px 0;
+  max-width: 100%;
+  overflow-x: hidden;
+
+  & > .actionrow {
+    padding: 0 0 20px 20px;
+  }
+
+  & .action-hidden {
+    visibility: hidden;
+  }
+`;
+
+const DataTables = () => {
+  const [rows, setRows] = useState(jsonData);
+  const [rowsHidden, setRowsHidden] = useState(() => {
+    return !localStorage.hiddenThreads
+      ? []
+      : localStorage.hiddenThreads.split(",");
+  });
+
+  const handleSorting = newData => {
+    return newData;
+  };
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch("src/components/DataTables/threads.json")
+        .then(res => {
+          return res.json();
+        })
+        .then(newData => {
+          newData = handleSorting(newData);
+          rows !== newData && setRows(newData);
+        });
+    }, 2000);
+  }, []);
+
+  const handleThread = (e, clearHidden) => {
+    if (clearHidden) {
+      document.querySelectorAll(".wrapper-datarow").forEach(el => {
+        el.classList.remove("hidden");
+      });
+      setRowsHidden([]);
+      localStorage.removeItem("hiddenThreads");
+      return;
+    }
+
+    let threadURL = e.target
+      .closest(".wrapper-datarow")
+      .querySelector(".datarow-source")
+      .getAttribute("href");
+
+    e.target.closest(".wrapper-datarow").classList.toggle("hidden");
+
+    setRowsHidden(() => [...rowsHidden, threadURL]);
+
+    localStorage.setItem("hiddenThreads", [...rowsHidden, threadURL]);
+  };
+
+  // removes URL no longer in json file list
+  useLayoutEffect(() => {
+    if (localStorage.hiddenThreads) {
+      rowsHidden.forEach(rowHidden => {
+        if (!rows.map(row => row.source == rowHidden).includes(true)) {
+          let updateHiddeRows = rowsHidden;
+          updateHiddeRows.splice(rowsHidden.indexOf(rowHidden), 1);
+          setRowsHidden(updateHiddeRows);
+          localStorage.setItem("hiddenThreads", [rowsHidden]);
+        }
+      });
+    }
+  }, [rows]);
+
+  // toggle info div
+  const toggleInfo = row => {
+    document.querySelectorAll(".datarow-info").forEach(info => {
+      info.remove();
+      document.querySelector("body").style.overflow = "auto";
+    });
+
+    if (row.close) {
+      document.querySelector(".overlay").remove();
+      document.querySelector("body").style.backgroundColor = "#fff";
+      return;
+    } else {
+      document.querySelector("body").style.overflow = "hidden";
+
+      let overlayStyle =
+        "background: rgba(0, 0, 0, 0.5); position: absolute; min-width: 100vw; min-height: 100vh; top: 0; z-index: 1";
+
+      let overlay = `<div class="overlay" style="${overlayStyle}"></div>`;
+
+      document.querySelector("body").insertAdjacentHTML("afterbegin", overlay);
+    }
+
+    row.e.target.closest(".wrapper-datarow").insertAdjacentHTML(
+      "afterend",
+      `
+      <div class="container-fluid datarow-info py-5">
+        <button id="info-close">
+          <i class="fa fa-times-circle" aria-hidden="true"></i>
+        </button>
+        ${row.info}
+      </div>
+    `
+    );
+
+    document.querySelector("#info-close").addEventListener("click", () => {
+      toggleInfo({ close: true });
+    });
+  };
+
   return (
-    <div>
-      <DataRows />
-    </div>
+    <Table>
+      <div className="row actionrow">
+        <small className={`col-3 ${rowsHidden.length < 1 && "action-hidden"}`}>
+          <a href="#" onClick={e => handleThread(e, true)}>
+            Show all hidden threads [{!rowsHidden ? "0" : rowsHidden.length}]
+          </a>
+        </small>
+      </div>
+      <DataRows
+        dataRow={rows}
+        dataHidden={rowsHidden}
+        isVisible={e => handleThread(e)}
+        toggleInfo={e => toggleInfo(e)}
+      />
+    </Table>
   );
 };
 
